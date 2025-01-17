b3fe31b51d90b0c2342dfa292f559cdf7520158d
Anjali Yadav <145827147+Vedanjalee@users.noreply.github.com>
2025-01-18 01:52:16 +0530

    Add files via upload

---
+++ Ternary_Operator.java
@@ -0,0 +1,10 @@
+
+public class Ternary_Operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20 ;
+        int big = (a>b)?a:b;
+
+        System.out.println("biggest number is : "+big);
+    }
+}
---
+++ Ternary_small.java
@@ -0,0 +1,11 @@
+public class Ternary_small {
+    
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 20 ;
+            
+    
+            System.out.println("small number is : "+ ((a<b)?a:b));
+        }
+    }
+    
