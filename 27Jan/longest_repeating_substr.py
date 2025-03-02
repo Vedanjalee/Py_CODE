219c81c619e27c516fc4cb16f06324c71705d346
Anjali Yadav <anjaliyadavp1@gmail.com>
2025-03-02 23:49:55 +0530

    "Longest Repeating Substr"

--- 27Jan/longest_repeating_substr.py
+++ 27Jan/longest_repeating_substr.py
@@ -3,4 +3,13 @@ def longest_repeating_substr(s) :
     max_len = 0 
 
     for i in range(n) :
-        for j in range()
+        for j in range(i+1, n+1):
+
+            substring = s[i:j] 
+            if substring in s[j:]:
+                max_len = max(max_len , len(substring))
+    return max_len 
+
+s = "banana"            
+print(longest_repeating_substr(s) )
+            
