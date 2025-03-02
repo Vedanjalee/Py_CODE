df2e9131d7dbeb584c0cf35930bcbc8ce335972d
Anjali Yadav <anjaliyadavp1@gmail.com>
2025-03-02 23:47:59 +0530

    "lOngest Substr distinct char"

---
+++ Longes_substr-DISTINCT_CHAR.py
@@ -0,0 +1,12 @@
+# def longest_unique_substring(s):
+#     n = len(s)
+#     longest = ""
+
+#     for i in range(n):
+#         seen_chars =set()
+#         for j in range(i, n):
+#             if s[j] in seen_chars:
+#                 break 
+#             seen_chars.add(s[j])
+#             if 
+
